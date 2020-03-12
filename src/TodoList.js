db3a862aa0d967c41846c47c24615c1d3f085939
Bryant Vaughn <bf_vaughn@hotmail.com>
2020-03-12 13:38:22 -0600

    delete and edit functionality

--- src/TodoList.js
+++ src/TodoList.js
@@ -6,9 +6,11 @@ class TodoList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      todos: [{ task: "Walk The Fish" }, { task: "Groom Chickens" }]
+      todos: []
     };
     this.create = this.create.bind(this);
+    this.remove = this.remove.bind(this);
+    this.update = this.update.bind(this);
   }
 
   create(newTodo) {
@@ -17,9 +19,35 @@ class TodoList extends Component {
     });
   }
 
+  remove(id) {
+    this.setState({
+      todos: this.state.todos.filter(t => t.id !== id)
+    })
+  }
+
+  update(id, updatedTask) {
+    const updatedTodos = this.state.todos.map(todo => {
+      if(todo.id === id) {
+        return { ...todo, task: updatedTask };
+      }
+      return todo;
+    });
+    this.setState({
+      todos: updatedTodos
+    });
+  }
+
   render() {
     const todos = this.state.todos.map(todo => {
-      return <Todo task={ todo.task } />;
+      return (
+        <Todo
+          key={ todo.id }
+          id={ todo.id }
+          task={ todo.task }
+          removeTodo={ this.remove }
+          updateTodo={ this.update }
+        />
+      );
     });
     return (
       <div>
